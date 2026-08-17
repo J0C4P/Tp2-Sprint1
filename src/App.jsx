a2f67658281bcf756b1a2f3fa106b50a48456c07
J0C4P <j0c4pa1bar@gmail.com>
2026-08-16 22:40:27 -0300

    aplicar estilos al resto de la app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 import Tracking from './components/Tracking'
@@ -10,17 +6,30 @@ import Feedback from './components/feedback/Feedback'
 import Footer from './components/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    <Navbar></Navbar>
-    <Hero></Hero>
-    <Menu></Menu>
-    <Tracking></Tracking>
-    <Feedback></Feedback>
-    <Footer></Footer>
-    </>
+    <div 
+      className="min-h-screen antialiased overflow-x-hidden"
+      style={{
+        backgroundColor: '#121214',
+        backgroundImage: "url(\"data:image/svg+xml,%3Csvg viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.85' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)' opacity='0.05'/%3E%3C/svg%3E\")",
+        color: '#e5e1e4',
+        fontFamily: 'var(--font-family-body)'
+      }}
+    >
+      <Navbar />
+
+      <main>
+        <Hero />
+
+        <Menu />
+
+        <Tracking />
+
+        <Feedback />
+      </main>
+
+      <Footer />
+    </div>
   )
 }
 
